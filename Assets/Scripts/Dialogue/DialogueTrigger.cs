744443eac90a409f0a142d1d5e24b3568895cb95
Matvey Shchetinin <mxs1466@student.bham.ac.uk>
2023-03-09 20:57:11 +0000

    Improves non-story dialogue, adds in-world sprites for conversations

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -7,6 +7,13 @@ public class DialogueTrigger : MonoBehaviour
 	[SerializeField] private Conversation standardConversation;
 	[SerializeField] private Conversation storyConversation;
 	[SerializeField] private Collider triggerCollider;
+	[SerializeField] private GameObject inWorldSpeaker;
+	[SerializeField] private GameObject inWorldStorySpeaker;
+
+	private void Start() 
+	{
+		RemoveWorldSpeaker(!OptionsManager.Instance.IsStoryMode());
+	}
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,16 +23,30 @@ public class DialogueTrigger : MonoBehaviour
 		}
     }
 
+	private void RemoveWorldSpeaker(bool storySpeaker)
+	{
+		if (storySpeaker && inWorldStorySpeaker)
+		{
+			Destroy(inWorldStorySpeaker);
+		}
+		else if (!storySpeaker && inWorldSpeaker)
+		{
+			Destroy(inWorldSpeaker);
+		}
+	}
+
     public void TriggerDialogue()
 	{
 		if (!DialogueManager.Instance.inConversation)
 		{
 			if (OptionsManager.Instance.IsStoryMode() && storyConversation != null)
 			{
+				RemoveWorldSpeaker(true);
 				DialogueManager.Instance.StartConversation(storyConversation);
 			}
 			else if (standardConversation != null)
 			{
+				RemoveWorldSpeaker(false);
 				DialogueManager.Instance.StartConversation(standardConversation);
 			}
 			triggerCollider.enabled = false;
